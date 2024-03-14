16c525b41178d3a8f8e5d7a11e8f447c3abaa6a7
garmanbeau <106044681+garmanbeau@users.noreply.github.com>
2024-03-14 10:49:42 -0500

    Initial Commit
    Created a Java project with maven.

---
+++ AssignTrack/src/main/java/com/se470/assigntrack/AssignTrack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.se470.assigntrack;
+
+/**
+ *
+ * @author garma
+ */
+public class AssignTrack {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
